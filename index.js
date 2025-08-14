a251705d7e7bf725176b851b3d03ca31034df2e4
Stefany2510 <Stefany2510>
2025-08-14 11:57:25 -0300

    novas rotas PUT e POST

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+let usuarios = []
+let contador = 1
+
 app.use(express.json())
 
 //ROTA DE TESTE
@@ -12,4 +15,37 @@ app.get("/status", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
+
+//ROTA GET Listar todos os usuários
+app.get("/usuarios", (req,res) => {
+  res.json(usuarios)
+})
+
+//ROTA POST Criar novo usuário
+app.post("/usuarios", (req, res) => {
+  const {nome} = req.body
+
+  const novoUsuario = {
+    id: contador++,
+    nome
+  }
+
+  usuarios.push(novoUsuario);
+  res.status(201).json(novoUsuario);
+})
+
+//ROTA PUT Atualiza usuario por ID
+app.put("/usuarios/:id", (req, res) => {
+  const id = parseInt(req.params.id)
+  const { nome } = req.body
+
+  const usuario = usuarios.find((i) => i.id === id)
+
+  if (!usuario) {
+    return res.status(404).json({ erro: "Usuário não encontrado"})
+  }
+
+  usuario.nome = nome
+  res.json(usuario)
 })
